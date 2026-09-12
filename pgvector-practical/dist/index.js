91a40b0fca8ad242821110e3417fbb5797ec3b98
Abhinay Jangde <64852930+abhinayjangde@users.noreply.github.com>
2026-09-12 06:35:12 +0000

    Add new HTML quote pages and initial web scraping notebook
    - Created quote5.html, quote6.html, and quote7.html with various quotes and authors.
    - Added a new Jupyter notebook (web_scraping.ipynb) for scraping data from HTML files using BeautifulSoup.
    - Implemented data extraction logic to gather names and populations from the scraped HTML.
    - Saved the extracted data into a CSV file (data.csv) for further analysis.

--- pgvector-practical/dist/index.js
+++ pgvector-practical/dist/index.js
@@ -1,7 +1,16 @@
+// import ollama from 'ollama'
+// const response = await ollama.chat({
+//   model: 'deepseek-r1:1.5b',
+//   messages: [{ role: 'user', content: 'Explain quantum computing in simple terms.' }],
+//   stream: true,
+// })
+// for await (const part of response) {
+//   process.stdout.write(part.message.content);
+// }
 import ollama from 'ollama';
-const response = await ollama.chat({
-    model: 'deepseek-r1:1.5b',
-    messages: [{ role: 'user', content: 'hello' }],
+const single = await ollama.embed({
+    model: 'embeddinggemma:latest',
+    input: 'hello',
 });
-console.log(response.message.content);
+console.log(single.embeddings); // vector
 //# sourceMappingURL=index.js.map
